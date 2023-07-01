a3a56fa580d2b56669e58237e4c1cb081a7196df
julioceno <julioreplay@gmail.com>
2023-07-01 10:03:14 -0300

    [fix] corrige error prisma migration run before container is up

---
+++ apps/single-sign-on/api/prisma/run-test.sh
@@ -0,0 +1,1 @@
+docker-compose -f ./docker/docker-compose.test.yml up -d && sleep .5 && yarn prisma migrate dev &&  yarn prisma db seed &&  jest --config ./test/jest-e2e.json ./test -i --detectOpenHandles; docker rm -f dirigir-more-single-sign-on-test-db
--- apps/single-sign-on/api/run-test.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./docker/docker-compose.test.yml up -d && yarn prisma migrate dev &&  yarn prisma db seed &&  jest --config ./test/jest-e2e.json ./test -i --detectOpenHandles; docker rm -f dirigir-more-single-sign-on-test-db
