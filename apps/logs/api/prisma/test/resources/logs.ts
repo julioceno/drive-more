76b648b2c8163a55cd57db98ea71a675b42ee0c0
Júlio Nepomuceno <julioreplay@gmail.com>
2023-08-09 19:05:26 -0300

    [feature]: create test e2e for logs system

---
+++ apps/logs/api/prisma/test/resources/logs.ts
@@ -0,0 +1,17 @@
+import { Prisma } from '@prisma/client';
+import { prisma } from '../seed';
+
+const data: Prisma.LogCreateManyInput[] = [
+  {
+    id: '9218d387-01af-4755-8a13-a7f13b79a753',
+  },
+];
+
+export async function logs() {
+  const { count } = await prisma.log.createMany({
+    data,
+    skipDuplicates: true,
+  });
+
+  console.log(`${count} logs createds 🚀`);
+}
