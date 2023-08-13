b3ca55c4425fd691fe19a7302de96cc0281c18ac
Júlio Nepomuceno <julioreplay@gmail.com>
2023-08-13 10:28:11 -0300

    [fix]

--- single-sign-on/api/prisma/resources/roles.ts
+++ single-sign-on/api/prisma/resources/roles.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client';
 import { prisma } from '../seed';
-import { RoleEnum } from '@/common';
+import { RoleEnum } from '../../src/common';
 
 const data: Prisma.RoleCreateManyInput[] = [
   {
