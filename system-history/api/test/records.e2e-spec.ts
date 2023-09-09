7d8c2eea1fa9900b4d95421556c6bfca1493b32e
Júlio Nepomuceno <julioreplay@gmail.com>
2023-09-09 13:40:04 -0300

    [feature]: create new test e2e

--- system-history/api/test/records.e2e-spec.ts
+++ system-history/api/test/records.e2e-spec.ts
@@ -12,6 +12,16 @@ describe('RecordsController (e2e)', () => {
 
   const baseUrl = '/records';
 
+  const payload = {
+    'Identificador Técnico': '2929c915-acaf-4773-aaad-caa67c1f8bbd',
+    Código: 21,
+    Nome: 'test',
+    Email: 'test21@dirigir.more.com',
+    Papel: 'Usuário',
+    'Data de Criação': '2023-09-09T16:14:23.842Z',
+    'Data de Atualização': '2023-09-09T16:14:23.842Z',
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [RecordsModule],
@@ -32,16 +42,6 @@ describe('RecordsController (e2e)', () => {
   });
 
   it('/ (POST)', async () => {
-    const payload = {
-      'Identificador Técnico': '2929c915-acaf-4773-aaad-caa67c1f8bbd',
-      Código: 21,
-      Nome: 'test',
-      Email: 'test21@dirigir.more.com',
-      Papel: 'Usuário',
-      'Data de Criação': '2023-09-09T16:14:23.842Z',
-      'Data de Atualização': '2023-09-09T16:14:23.842Z',
-    };
-
     const response = await request(app.getHttpServer()).post(baseUrl).send({
       creatorEmail: 'user@dirigir.more.com',
       action: Action.CREATE,
@@ -53,7 +53,6 @@ describe('RecordsController (e2e)', () => {
 
     expect(response).toBeDefined();
     expect(response.status).toBe(201);
-
     expect(response.body).toStrictEqual({
       action: Action.CREATE,
       codigo: expect.any(Number),
@@ -63,6 +62,30 @@ describe('RecordsController (e2e)', () => {
       id: expect.any(String),
       payload,
       resourceId: '9218d387-01af-4755-8a13-a7f13b79a753',
+    });
+  });
+
+  it('/ (POST -> Create new Module and new Resource, before create history record)', async () => {
+    const response = await request(app.getHttpServer()).post(baseUrl).send({
+      creatorEmail: 'user@dirigir.more.com',
+      action: Action.CREATE,
+      entityId: '1',
+      payload: payload,
+      moduleName: 'CORE',
+      resourceName: 'MAIN',
+    });
+
+    expect(response).toBeDefined();
+    expect(response.status).toBe(201);
+    expect(response.body).toStrictEqual({
+      action: Action.CREATE,
+      codigo: expect.any(Number),
+      createdAt: expect.any(String),
+      creatorEmail: 'user@dirigir.more.com',
+      entityId: '1',
+      id: expect.any(String),
+      payload,
+      resourceId: expect.any(String),
     });
   });
 });
