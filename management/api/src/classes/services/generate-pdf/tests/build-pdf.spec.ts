d1aed862b1dfce052d40c6c0d425c782235de2b8
Júlio Nepomuceno <julioreplay@gmail.com>
2023-10-31 22:57:42 -0300

    [fix]

--- management/api/src/classes/services/generate-pdf/tests/build-pdf.spec.ts
+++ management/api/src/classes/services/generate-pdf/tests/build-pdf.spec.ts
@@ -27,6 +27,18 @@ jest.mock('node:worker_threads', () => {
 
 jest.mock('puppeteer');
 
+const puppeteer = require('puppeteer');
+
+puppeteer.launch.mockImplementation(() => ({
+  newPage: jest.fn().mockImplementation(() => ({
+    setContent: jest.fn().mockResolvedValue({}),
+    emulateMediaType: jest.fn().mockResolvedValue({}),
+    pdf: jest.fn().mockResolvedValue({}),
+    close: jest.fn().mockResolvedValue({}),
+  })),
+  close: jest.fn().mockResolvedValue({}),
+}));
+
 describe('BuildPdf', () => {
   it('', async () => {
     jest.mock('puppeteer');
@@ -35,18 +47,22 @@ describe('BuildPdf', () => {
     const pdfMock = jest.fn().mockResolvedValue({});
 
     puppeteer.launch.mockImplementation(() => ({
-      newPage: jest.fn().mockResolvedValue({
+      newPage: jest.fn().mockImplementation(() => ({
         setContent: jest.fn().mockResolvedValue({}),
         emulateMediaType: jest.fn().mockResolvedValue({}),
         pdf: pdfMock,
         close: jest.fn().mockResolvedValue({}),
-      }),
+      })),
       close: jest.fn().mockResolvedValue({}),
     }));
 
     await new BuildPdf().run();
 
     // Verifica se a função 'pdf' foi chamada
-    expect(pdfMock).toHaveBeenCalledWith({});
+    expect(pdfMock).toHaveBeenCalledWith({
+      format: 'A4',
+      path: expect.stringContaining('./src/public/'),
+      printBackground: true,
+    });
   });
 });
